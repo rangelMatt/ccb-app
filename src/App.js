75dd4a5a8e67e75b6f2e41ce925479105a31f543
Matt Rangel <mattrangel@gmail.com>
2022-11-08 13:44:48 -0800

    header complete

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import {  useState } from "react";
+import { useState } from "react";
 import "./App.scss";
-import Modal from "./components/Modal";
+import Header from "./components/header"
+import EquipModal from "./components/EquipModal";
 import data from "./services/data"
 
 
 
 function App() {
-  const [state, setState] = useState()
-  const [modal, setModal] = useState(false);
-  const Toggle = () => setModal(!modal);
+  // const [state, setState] = useState()
+  // const [modal, setModal] = useState(false);
+  // const Toggle = () => setModal(!modal);
 
-  const onChangeCombobox = (e) => {
-    const selectedId = e.target.value;
-    const selectedEquipState = data.filter((d) => d.id === selectedId)[0];
-    console.log(selectedEquipState)
-    setState(selectedEquipState)
-  }
+  // const onChangeCombobox = (e) => {
+  //   const selectedId = e.target.value;
+  //   const selectedEquipState = data.filter((d) => d.id === selectedId)[0];
+  //   console.log(selectedEquipState)
+  //   setState(selectedEquipState)
+  // }
 
   // useEffect(() => {
   //   console.log(setState(data));
@@ -23,41 +24,9 @@ function App() {
 
   return (
     <div className="App">
-      <div className="wrapper"
+      <Header/>
+      <EquipModal/>
         
-        onChange={(e) => {
-          onChangeCombobox(e);
-        }}
-      >
-        {data && data.map((d) => (
-          <button
-            className="clickMe"
-            onClick={() => {
-              setState(d)
-              Toggle()} } 
-            key={d.id}
-            value={d.id}
-            data-toggle="modal"
-          >
-            {d.name}
-          </button>
-        ))}
-      </div>
-
-        <Modal 
-          style={{height: '200px'}}
-          show={modal}
-          // title={state.name}
-          // title={data.title}
-          // children={data.desc}
-
-          title={state?.name}
-          children={state?.desc}
-          src={state?.src}
-          link={state?.link}
-          id={state?.id}
-          close={Toggle}></Modal>
-
     </div>
   );
 }
