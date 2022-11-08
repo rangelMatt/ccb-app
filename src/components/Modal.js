1a06119b758a5f9b8d0b87f5131739ceeaee64e4
Matt Rangel <mattrangel@gmail.com>
2022-11-08 09:24:22 -0800

    completed modal in sandbox environment

---
+++ src/components/Modal.js
@@ -0,0 +1,50 @@
+import ReactDom from "react-dom";
+import "./modal.scss";
+import Close from "../images/times-close-circle.png";
+import advChest from "../images/advanced-chest.jpg";
+import React from "react";
+
+
+
+
+const Modal = ({ show, close, title, children, src, id,link }) => {
+    
+
+  return ReactDom.createPortal(
+    <>
+      <div
+        className={`modalContainer ${show ? "show" : ""} `}
+        onClick={() => close()}
+      >
+        <div className="modal" onClick={(e) => e.stopPropagation()}
+        key={id}>
+          <header className="modal_header">
+            <a href={link}>
+            <h2 className="modal_header-title">{title}</h2>
+            </a>
+            <button className="close" onClick={() => close()}>
+              <img src={Close} alt="close" />
+            </button>
+          </header>
+          <main className="modal_content"
+            style={{ display: "flex" }}>
+            <div> {}
+              <img className="image" src={src} style={{ width: "100%" }} alt="" />
+            </div>
+            <div className="description text-break scroller"
+            >{children}</div>
+          </main>
+          <footer className="modal_footer">
+            <button className="modal-close" onClick={() => close()}>
+              Cancel
+            </button>
+            <button className="submit">Submit</button>
+          </footer>
+        </div>
+      </div>
+
+    </>,
+    document.getElementById("modal")
+  );
+}
+export default Modal;
---
+++ src/services/data.js
@@ -0,0 +1,62 @@
+import advChest from "../images/advanced-chest.jpg";
+
+const data = [
+  {
+    id: 1,
+    name: "Icarian Standing Deltoid Raise",
+    desc: "We have been very excited to get this Rare piece in our Gym. Very hard to find, Icarian machine. We’ve been trying to find one for almost a year, being told it’s impossible, but we got one to fit in our Gym! Overall smooth Machine.\nDefinitely feels a better connection with the lateral(middle) part of the Shoulder in comparison to using Dumbbells as a variation.\nDefinitely feel a good connection with it in the Side Delts.",
+    src: require("../images/standing-delt-raise.jpg"),
+    link: "https://www.instagram.com/tv/Cfg96PClFgC/?igshid=YmMyMTA2M2Y= "
+  },
+  {
+    id: 2,
+    name: "FLEX Fitness LEVERAGE Chest Press",
+    desc: "Rare & Highly Sought after, FLEX Leverage Chest Press. Part of the Rare Oldschool Flex Leverage line, that sponsored much of the top Olympia pros back in the day.\nThe design of these machines compared to typical plate loaded machines, is that Flex Leverage machines have constant, even tension all throughout the movement.\nTypical Plate Loaded Machines, as the weight physically gets closer to the joint of the movement, the strength curve lessens, so at some point of the movement the weight load lessens. That’s not the case with the design of these Oldschool Flex Leverage pieces.\nThe converging at the top of the movement, feels a great squeeze on the chest as it tracks inward. Feedback on this machine has been one of the best in our Facility. Great Chest connection and Pump, feels great overall.",
+    src: require("../images/leverage-chest-press.jpg"),
+    link: "https://www.instagram.com/tv/Cey5s16pUnh/?igshid=YmMyMTA2M2Y="
+  },
+  {
+    id: 3,
+    name: "CYBEX Advanced Chest",
+    desc: "Got ourselves another Rare Machine, Cybex Advanced Chest.\nThis machine is like a combination of a Chest Fly and Press. The machine is free moving, compared to a fixed range of motion on most machines.\nThe stretch and squeeze at the top is great and especially for doing slow and controlled movements. This machine is definitely harder to load with weight, and works better doing a Moderate weight for Best Mind-Muscle Connection.\nVery glad to have finally found this piece Flex Wheeler and Chris Cormier would use in their Olympia prep during the 90s.",
+    src: require("../images/advanced-chest.jpg"),
+    link: "https://www.instagram.com/p/CWMEmsdvpyb/?igshid=YmMyMTA2M2Y="
+  },
+  {
+    id: 4,
+    name: "Nautilus First-Generation Super Pullover",
+    desc: "Hard to find, and a very Underrated Machine/Exercise for Lat Development, you typically do not find in most Gyms. Even if you can find a Pullover Machine, none compare to the original Nautilus Pullover. There’s a couple newer Nautilus Nitro versions and Hammer Strength ones we have tried, but the spacing between the handle and elbow pads have always been awkward, the spacing on this one feels great.\nWhen I personally began to incorporate our Pullover Machine on my last Back Training session, it definitely made me sore. It definitely is an underrated back exercise.\nThe whole purpose of this machine compared to Dumbbell Pullovers, or Cable Pullovers(one of my favorite back exercises), Is the Range of Motion you get and really gives you a good stretch and squeeze of your Lats, that you can’t really achieve without the machine version.\nThe machine version completely eliminates the involvement of the biceps, so it completely isolates your back, compared to doing rows or Pulldown movements.\nDefinitely using an underhand grip works best. Using the belt, which I overlooked at first, definitely makes a difference in the feel as you go heavier. Invisible Lat Syndrome is real after hitting this Machine.",
+    src: require("../images/super-pullover.jpg"),
+    link: "https://www.instagram.com/tv/CVj451FlCyG/?igshid=YmMyMTA2M2Y="
+
+  },
+  {
+    id: 5,
+    name: "FLEX Fitness Seated Tricep Dip",
+    desc: "Part of the Oldschool Flex Fitness  line, that sponsored much of the top Olympia pros back in the day.\nGreat Accessory/Isolation Exercise for Triceps Development/Strength. Capable of widening the grip to lean the focus more on the Lower Chest.",
+    src: require("../images/seated-tricep-dip.jpg")
+  },
+  {
+    id: 6,
+    name: "Icarian Hack Squat (35° angle)",
+    desc: "One of the Best Feeling oldschool Hack Squats out there, to grow some Mighty Tree Trunk Legs.    \nThe Angle on this Particular Hack Squat is at a lower tilt in comparison to most Hack Squat machines, which are at a 45° angle. Meaning, this has less pressure on the knees in comparison. The platform size is great to change up Footstance Variation to switch up focus on varying parts of the Legs.\nThe handles at the bottom to unrack the weight, make it great to hold onto. While performing the exercise, it allows you to use less of your back while driving up, and maintain the Weight Load on the legs, instead of driving more with your back.\nThe design of this machine also works great for using our Bands to perform Reverse Band Hack Squats.",
+    src: require("../images/hack-squat.jpg")
+  },
+  {
+    id: 7,
+    name: "Icarian Donkey Calf Raise",
+    desc: "An Awesome oldschool Calf Machine not commonly seen in most Gyms. Personally one of my Favorite Calf Machines in our arsenal, the stretch on this machine feels great! This Machine is fairly easy to Load up with weight, as we have our Gym Pin to load up weight with 45s, past Maxing out the 400lb Stack.",
+    src: require("../images/donkey-calfraise.jpg"),
+    link: "https://www.instagram.com/tv/CeYz7mOl3hN/?igshid=YmMyMTA2M2Y="
+  },
+  {
+    id: 8,
+    name: "Hammer Strength Seated Leg Curl (Plate Loaded)",
+    desc: "Plate Loaded Version of the Seated Hamstring Curl Machine. This machine is great for Priming up the Hamstrings for your Leg Training Session and also as a Finisher.\nThe plate loaded version definitely has a great squeeze as there is less friction than the Selectorized (weight stack) Version. The handle on this machine is great for doing forced reps to really burn out the Hamstrings and assist with reps.",
+    src: require("../images/hs-seated-leg-curl.jpg"),
+    link: "https://www.instagram.com/p/CQJvnfCHbkm/?igshid=YmMyMTA2M2Y="
+  },
+];
+
+export default data;
+
