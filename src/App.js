ed582e93fa9e3cc86cb0467d54a7411b9e170ef6
Matt Rangel <mattrangel@gmail.com>
2022-11-08 14:11:33 -0800

    deleted white space

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-import { useState } from "react";
+// import { useState } from "react";
 import "./App.scss";
 import Header from "./components/header"
 import EquipModal from "./components/EquipModal";
-import data from "./services/data"
+// import data from "./services/data"
 
 
 
 function App() {
-  // const [state, setState] = useState()
-  // const [modal, setModal] = useState(false);
-  // const Toggle = () => setModal(!modal);
-
-  // const onChangeCombobox = (e) => {
-  //   const selectedId = e.target.value;
-  //   const selectedEquipState = data.filter((d) => d.id === selectedId)[0];
-  //   console.log(selectedEquipState)
-  //   setState(selectedEquipState)
-  // }
-
-  // useEffect(() => {
-  //   console.log(setState(data));
-  // }, []);
 
   return (
     <div className="App">
