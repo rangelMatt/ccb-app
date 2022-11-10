49c00b7c6a8ce7f52309b3ba0d784e5a3c5b9a4a
Matt Rangel <mattrangel@gmail.com>
2022-11-10 12:11:12 -0800

    styled header pictures

--- src/components/header.js
+++ src/components/header.js
@@ -4,6 +4,9 @@ import styles from "./signup.module.css"
 import SignUp from "./SignUp"
 import DayPass from "./DayPass"
 import ccbLogo from "../images/ccb-logo.jpeg"
+import icarian from "../images/icarian-logo.png"
+import hammerStrength from "../images/hammer-strength.png"
+import cybex from "../images/cybex-logo-and-tagline.jpg"
 
 import { HiOutlineSpeakerphone, HiOutlineKey } from "react-icons/hi";
 
@@ -14,23 +17,23 @@ function Header() {
     alert(message);
   }, []);
 
-  const stickyHeader = useRef()
-  useLayoutEffect(() => {
-    const mainHeader = document.getElementById('App-header');
-    let fixedTop = stickyHeader.current.offsetTop
-    const fixedHeader = () => {
-      if (window.pageYOffset > fixedTop) {
-        mainHeader.classList.add('fixedTop')
-      } else {
-        mainHeader.classList.remove('fixedTop')
-      }
-    }
-    window.addEventListener('scroll', fixedHeader)
-  }, [])
+  // const stickyHeader = useRef()
+  // useLayoutEffect(() => {
+  //   const mainHeader = document.getElementById('App-header');
+  //   let fixedTop = stickyHeader.current.offsetTop
+  //   const fixedHeader = () => {
+  //     if (window.pageYOffset > fixedTop) {
+  //       mainHeader.classList.add('fixedTop')
+  //     } else {
+  //       mainHeader.classList.remove('fixedTop')
+  //     }
+  //   }
+  //   window.addEventListener('scroll', fixedHeader)
+  // }, [])
   return (
     <div>
       <hr className="line"></hr>
-      <div className="App-header" id="App-header" ref={stickyHeader}>
+      <div className="App-header" id="App-header" >
 
 
         <header >
@@ -47,12 +50,18 @@ function Header() {
         </header>
       </div>
       <hr className="line"></hr>
-      <div>
+      <div className="gorilla-rack">
         <img
-          className="gorilla-rack"
+          
           src={GorillaRack}
           alt="gorilla-rack"
         ></img>
+
+      </div>
+      <div className="logos">
+        <img src={icarian} alt=""></img>
+        <img src={hammerStrength} alt=""></img>
+        <img src={cybex} alt=""></img>
       </div>
 
       <hr className="line"></hr>
@@ -77,10 +86,10 @@ function Header() {
 
         </div>
         <div className="about-gym">
-          <h3>Central Coast Barbell is an oldschool, 24-hour training facility. Our Office is open Monday-Friday 4:30 PM to 7:00 PM for New member Sign-ups.<br/>
-          <br/>
+          <h3>Central Coast Barbell is an oldschool, 24-hour training facility. Our Office is open Monday-Friday 4:30 PM to 7:00 PM for New member Sign-ups.<br />
+            <br />
             We offer Free Weights, Specialty Bars, Selectorized (cable) Machines, Plate Loaded Machines, Chalk, Cardio Equipment, Powerlifting Equipment, Strongman Equipment.
-            <br/><br/>
+            <br /><br />
             Central Coast Barbell strives to offer a Focused & Supportive environment/community for all fitness levels.</h3>
         </div>
       </div>
