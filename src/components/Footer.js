55e878adf02027870d394d35aec7d82e240bf2f3
Matt Rangel <mattrangel@gmail.com>
2022-11-21 12:18:04 -0800

    started code zoom image in modal

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,10 +18,14 @@ function Footer() {
     <hr className="line"></hr>
       <div className="headers">
         <h1>Office Hours & Day Passes</h1>
+        <p className="about-gym">(Day and Week Passes include 24 Hour Access to Gym)</p>
         </div>
+        
         <hr className="line"></hr>
         <div className="office-content">
-          <h4><u>Day Passes:</u> $$$</h4>
+          
+          <h4><u>GymDay Pass:</u> $15</h4>
+          <h4><u>Week Pass:</u> $30</h4>
           <h4><u>Monday - Friday:</u> 4:30pm - 7:00pm</h4>
           <h4><u>Saturday & Sunday:</u> By Appointment Only</h4>
           </div>
@@ -55,7 +59,7 @@ function Footer() {
         
         <hr className="line"></hr>
         <div className="office-content">
-          <h4><u>Phone:</u> <a href="tel:+1-805-627-2329">805-627-2329</a></h4>
+          <h4><u>Phone:</u> <a href="tel:+1-805-489-1618">805-489-1618</a></h4>
           <h4><u>Address:</u> <a
             href="https://www.google.com/maps/place/Central+Coast+Barbell/@35.1092097,-120.6184355,21z/data=!4m13!1m7!3m6!1s0x80ec5ead667d5a27:0xdfac6f9659f20cc5!2s1081+Highland+Way+a,+Grover+Beach,+CA+93433!3b1!8m2!3d35.1092097!4d-120.6182988!3m4!1s0x80ec594b9187f103:0x1790f1c03fca0846!8m2!3d35.1092097!4d-120.6182987"
             target="_blank"
